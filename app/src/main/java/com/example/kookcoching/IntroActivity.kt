bb664b60b3c5d399e04af98851d965685b8cf8e6
seonghwan <nsh1998@kookmin.ac.kr>
2020-09-25 00:03:24 +0900

    Make intro part

---
+++ app/src/main/java/com/example/kookcoching/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kookcoching
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        startLoading()
+    }
+
+    private fun startLoading() {
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
