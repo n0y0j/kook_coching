226877c02769405722193c4a989a02bcc9e9d31e
n0y0j <n0y0j77@gmail.com>
2020-10-30 18:51:23 +0900

    feat: Upload image in FireStorage.

--- app/src/main/java/com/example/kookcoching/Fragment/Share/WriteBoardActivity.kt
+++ app/src/main/java/com/example/kookcoching/Fragment/Share/WriteBoardActivity.kt
@@ -1,32 +1,36 @@
 package com.example.kookcoching.Fragment.Share
 
-import android.Manifest
 import android.Manifest.permission.*
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.FragmentTransaction
-import com.example.kookcoching.Fragment.ShareBoardFragment
 import com.example.kookcoching.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
-import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import gun0912.tedbottompicker.TedBottomPicker
 import gun0912.tedbottompicker.TedBottomSheetDialogFragment
 
 class WriteBoardActivity : AppCompatActivity() {
+
+    private var mStorageRef: StorageReference? = null
+    private var fbFirestore: FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_board)
@@ -42,13 +46,14 @@ class WriteBoardActivity : AppCompatActivity() {
 
         var intent: Intent = getIntent()
         val chip_count: String? = intent.getStringExtra("chip_type")
+        var selectUrlList:List<Uri> = listOf()
 
         btn_camera.setOnClickListener{
 
             // 2020.10.30 / 노용준 / 앨범 권한에 대한 Permission
             var permissionlistener:PermissionListener = object: PermissionListener {
                 override fun onPermissionGranted() {
-                    var selectUrlList:List<Uri> = listOf()
+                    selectUrlList = listOf()
                     Toast.makeText(this@WriteBoardActivity, "권한 허가", Toast.LENGTH_SHORT).show()
 
                     TedBottomPicker.with(this@WriteBoardActivity)
@@ -57,25 +62,37 @@ class WriteBoardActivity : AppCompatActivity() {
                         .setCompleteButtonText("Done")
                         .setEmptySelectionText("No Select")
                         .setSelectedUriList(selectUrlList)
-                        .showMultiImage(object:TedBottomSheetDialogFragment.OnMultiImageSelectedListener {
-                            override fun onImagesSelected(uriList:List<Uri>) {
-                                Log.d("asd", uriList.toString())
+                        .showMultiImage(object :
+                            TedBottomSheetDialogFragment.OnMultiImageSelectedListener {
+                            override fun onImagesSelected(uriList: List<Uri>) {
+                                Log.d("image", uriList.toString())
+                                selectUrlList = uriList
                             }
                         })
 
                 }
-                override fun onPermissionDenied(deniedPermissions:List<String>) {
-                    Toast.makeText(this@WriteBoardActivity, "권한 거부" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+                override fun onPermissionDenied(deniedPermissions: List<String>) {
+                    Toast.makeText(
+                        this@WriteBoardActivity,
+                        "권한 거부" + deniedPermissions.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
-            ActivityCompat.requestPermissions(this@WriteBoardActivity, arrayOf<String>(READ_CONTACTS, WRITE_EXTERNAL_STORAGE),
-                1)
+            ActivityCompat.requestPermissions(
+                this@WriteBoardActivity, arrayOf<String>(
+                    READ_CONTACTS,
+                    WRITE_EXTERNAL_STORAGE,
+                    READ_EXTERNAL_STORAGE
+                ),
+                1
+            )
 
             TedPermission.with(this)
                 .setPermissionListener(permissionlistener)
                 .setDeniedMessage("If you reject permission,you can not use this service Please turn on permissions at [Setting] > [Permission]")
-                .setPermissions(READ_CONTACTS, WRITE_EXTERNAL_STORAGE)
+                .setPermissions(READ_CONTACTS, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE)
                 .check()
 
         }
@@ -94,7 +111,7 @@ class WriteBoardActivity : AppCompatActivity() {
                 }
         }
 
-        chipGroup.setOnCheckedChangeListener{ group, chech_pos:Int ->
+        chipGroup.setOnCheckedChangeListener{ group, chech_pos: Int ->
 
             val chip:Chip? = findViewById(chech_pos)
             tag = chip?.text.toString()
@@ -111,13 +128,36 @@ class WriteBoardActivity : AppCompatActivity() {
         btn_store.setOnClickListener {
             val title = findViewById(R.id.et_title) as EditText
             val content = findViewById<EditText>(R.id.et_content)
-            var fbFirestore: FirebaseFirestore? = null
+
+            val id = System.currentTimeMillis().toString()
 
             fbFirestore = FirebaseFirestore.getInstance() // firestore 인스턴스 초기화
+            mStorageRef = FirebaseStorage.getInstance().getReference()
+
+            for ( item in selectUrlList ) {
+                val temp = item.toString().split("/")
+                val name = temp[temp.size-1]
+
+                var imageRef = mStorageRef!!.child(id + "/" + name)
+                val uploadTask = imageRef.putFile(item)
+
+                uploadTask.continueWith{
+                    imageRef.downloadUrl
+                }.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        it.result!!.addOnSuccessListener {task ->
+                            var uri = task.toString()
+                            Log.d("asd", uri)
+                        }
+                    }
+                }
+            }
 
             // 2020.10.29 / 노용준 / document name을 epoch time으로 설정 (시간 순으로 자동정렬)
-            fbFirestore?.collection("share_post")?.document(System.currentTimeMillis().toString())
-                ?.set(Post(title.text.toString(),content.text.toString(), tag))
+//            fbFirestore?.collection("share_post")?.document(id)
+//                ?.set(Post(title.text.toString(), content.text.toString(), tag))
+            Log.d("asd", "hizzzzzzzzzzzzzzzzzzzzz")
+
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
             finish()
         }
