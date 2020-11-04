3bfb25b684ece961c1b25e3eee27579087881552
n0y0j <n0y0j77@gmail.com>
2020-11-04 15:43:04 +0900

    feat: Implement register xml.

---
+++ app/src/main/java/com/example/kookcoching/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kookcoching
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 2020.11.04 / 노용준 / 로그인 화면
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/kookcoching/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kookcoching
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 2020.11.04 / 노용준 / 회원가입 화면
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
