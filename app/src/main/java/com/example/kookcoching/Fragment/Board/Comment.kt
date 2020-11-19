0918a0de1d92277e66948773b8da1c98aa3daf00
Boyzmsc <boyzmsc@naver.com>
2020-11-19 16:28:25 +0900

    update comment

--- app/src/main/java/com/example/kookcoching/Fragment/Board/Comment.kt
+++ app/src/main/java/com/example/kookcoching/Fragment/Board/Comment.kt
@@ -1,6 +1,6 @@
 package com.example.kookcoching.Fragment.Board
 
-// 2020.10.26 / 문성찬 / 댓글 클래스
+// 2020.10.26 / 문성찬 / 댓글 클래스 작성
 data class Comment (var comment: String, var author: String, var nickname: String) {
 }
 
